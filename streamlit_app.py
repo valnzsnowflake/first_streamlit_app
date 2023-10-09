65b844987a2628b995cc9fce1237b3e4a8a584b2
Val Nz Snowflake <147032626+valnzsnowflake@users.noreply.github.com>
2023-10-09 19:50:52 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,5 +53,13 @@ if streamlit.button('Get Fruit Load List'):
 
 streamlit.stop()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
